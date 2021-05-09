25601379e573f2fc9b33c8a4d8ffae866d540a5b
streamich <streamich@gmail.com>
2021-05-09 21:39:19 +0200

    feat: 🎸 implement binary messages

---
+++ src/common/messages/nominal/types.ts
@@ -0,0 +1,20 @@
+import type {NotificationMessage} from "./NotificationMessage";
+import type {RequestCompleteMessage} from "./RequestCompleteMessage";
+import type {RequestDataMessage} from "./RequestDataMessage";
+import type {RequestErrorMessage} from "./RequestErrorMessage";
+import type {RequestUnsubscribeMessage} from "./RequestUnsubscribeMessage";
+import type {ResponseCompleteMessage} from "./ResponseCompleteMessage";
+import type {ResponseDataMessage} from "./ResponseDataMessage";
+import type {ResponseErrorMessage} from "./ResponseErrorMessage";
+import type {ResponseUnsubscribeMessage} from "./ResponseUnsubscribeMessage";
+
+export type ReactiveRpcMessage =
+  | NotificationMessage
+  | RequestDataMessage
+  | RequestCompleteMessage
+  | RequestErrorMessage
+  | RequestUnsubscribeMessage
+  | ResponseDataMessage
+  | ResponseCompleteMessage
+  | ResponseErrorMessage
+  | ResponseUnsubscribeMessage;
