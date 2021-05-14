7e329f14b43f46b341a81e4323374a8211645321
streamich <streamich@gmail.com>
2021-05-14 21:35:22 +0200

    feat: 🎸 add Encoder/Decoder classes for compact codec

--- src/common/codec/compact/encode.ts
+++ src/common/codec/compact/encode.ts
@@ -1,7 +1,7 @@
-import type {Message} from '../../messages/nominal/types';
+import type {ReactiveRpcMessage} from '../../messages/nominal/types';
 import type {CompactMessage} from './types';
 
-export function encode(messages: Message[]): CompactMessage[] {
+export function encode(messages: ReactiveRpcMessage[]): CompactMessage[] {
   const length = messages.length;
   const out: CompactMessage[] = [];
   for (let i = 0; i < length; i++) out.push(messages[i].toCompact());
